8b3da46efbd936ae7a35afff9ffc6a94df0bbcb7
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-24 16:32:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,14 @@ class Student < User
   attr_accessor :first_name, :last_name
   
   def initialize
-    
+    @knowledge = []
+  end
+  
+  def learn(lesson) 
+    @knowledge << lesson
+  end
+  
+  def knowledge
+    @knowledge
   end
 end
